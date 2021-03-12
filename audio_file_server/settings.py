ef2044253c6159ef7e7286a0e04f18546aa8407a
Adejoke <ogunyinkajoke@gmail.com>
2021-03-12 10:22:53 +0100

    resolved workflow problems

--- audio_file_server/settings.py
+++ audio_file_server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 import dj_database_url
 import django_heroku
 from pathlib import Path
@@ -82,6 +83,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql'
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
