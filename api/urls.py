8a3eaacdca567291df2338462cde5947c9ab22e5
Adejoke <ogunyinkajoke@gmail.com>
2021-03-15 06:50:27 +0100

    added README

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from .views import CreateView, GetView, GetAllView, UpdateView, DeleteView
 
 urlpatterns = [
     path('create', CreateView.as_view(), name='create_audio'),
-    path('get/<str:audioFileType>', GetAllView.as_view(), name='get_all'),
-    path('get/<str:audioFileType>/<int:audioFileID>', GetView.as_view(), name='get_audio'),
+    path('<str:audioFileType>', GetAllView.as_view(), name='get_all'),
+    path('<str:audioFileType>/<int:audioFileID>', GetView.as_view(), name='get_audio'),
     path('update/<str:audioFileType>/<int:audioFileID>', UpdateView.as_view(), name='update_audio'),
     path('delete/<str:audioFileType>/<int:audioFileID>', DeleteView.as_view(), name='delete_audio')
 ]
